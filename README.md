18eef949c3d580d8cd8ce9d5ca371b4e5f3cb584
Bryan351018 <Bryan351018@outlook.com>
2022-08-28 15:38:18 -0300

    First Git Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Propeller
+## A web-based, MIDI-compliant Digital Audio Workstation
+
+### Video Demo:  <URL HERE>
+### Description:
+TODO
+
+### Process
+1. While researching the libraries available, I found MIDI.js. Its tutorial works, but it is very undocumented and a little hard to use. So, I resorted to other libraries.
+
+### Libraries Used
++ soundfont-player
++ midi-file
++ FileSaver.js
---
+++ index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+// Audio context
+var ac;
+
+// Soundfont collection name (e.g. FluidR3_GM)
+const col_name = "FluidR3_GM";
+
+// Soundfont extension (ogg/mp3)
+var sf_ext = "ogg";
+
+// List of initialized instruments
+var instruments = {};
+
+// Parsed MIDI contents
+var MIDIContents;
+
+// Get soundfont name of a specific instrument
+function getSFName(insName)
+{
+    // insName: instrument name (e.g. accordion)
+    return "soundfonts/" + col_name + "/" + insName + "-" + sf_ext + ".js";
+}
+
+// Initialize audio context
+function audioCtxInit()
+{
+    ac = new AudioContext();
+}
+
+// Initialize an instrument
+function initIns(name)
+{
+    // Attempt to initialize
+    Soundfont.instrument(ac, getSFName(name)).then(
+        // Initialization successful
+        function(ins)
+        {
+            // Set instruments array
+            instruments[name] = ins;
+        }
+    ).catch(
+        // Initialization failed
+        function()
+        {
+            console.error("Could not initialize " + getSFName(name));
+        }
+    );
+}
+
+// Load and parse a MIDI file into MIDIContents
+function loadMIDIFile()
+{
+    // Get file
+    const file = document.getElementById("midi-file").files[0];
+    console.info("Loading \"" + file.name + "\" (" + file.size + " B)");
+    // Check for non-MIDIs
+    if (file.type != "audio/midi")
+    {
+        console.error("The inputted file is not a MIDI file");
+        return null;
+    }
+
+    // Read file
+    file.arrayBuffer().then(
+        // Read successful
+        function(contents)
+        {
+            console.log(contents);
+
+            // Must be seperate
+            MIDIContents = new Uint8Array(contents);
+            MIDIContents = parseMidi(MIDIContents);
+
+            console.info(MIDIContents);
+        }
+    ).catch(
+        // Read failed
+        function()
+        {
+            console.error("Failed to read MIDI file");
+        }
+    );
+}
+
+// Download a file (https://stackoverflow.com/questions/54626186/how-to-download-file-with-javascript)
+function downloadURI(uri, name)
+{
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    link.click();
+    link.remove();
+}
+
+// Parse MIDIContents and generate a downloadable MIDI File
+function saveMIDIFile(name)
+{
+    let rectifiedArray = new Uint8Array(writeMidi(MIDIContents));
+    console.log(rectifiedArray);
+
+    let blob = new File([rectifiedArray], {type: "audio/midi"});
+    console.info(blob);
+    saveAs(blob, name);
+}
